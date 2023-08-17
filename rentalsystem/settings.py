cf5994d5904c3719a6a09bb0e9ebfcc905b9bfd3
harumaru870PC <harumaru870@gmail.com>
2023-08-18 00:41:13 +0900

    修正

--- rentalsystem/settings.py
+++ rentalsystem/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-mzvscq5xq5_97qducsz83$l=m0xv(9pim$&0#0bhr!)fli^jqw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 # Or * to allow all
 
 
