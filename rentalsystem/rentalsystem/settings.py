f805e20bf21d734b63cc144a040a99c36e8d1fac
Haruhiro Seki <124242219+harumaru870@users.noreply.github.com>
2023-08-17 23:30:05 +0900

    Update settings.py

--- rentalsystem/rentalsystem/settings.py
+++ rentalsystem/rentalsystem/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-mzvscq5xq5_97qducsz83$l=m0xv(9pim$&0#0bhr!)fli^jqw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','vercel.app']
 
 
 # Application definition
